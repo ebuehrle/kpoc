1074d5417263627ffae3f8c281813ac04ef83d2e
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2025-07-02 17:44:52 +0200

    merge

--- merge.jl
+++ merge.jl
@@ -1,6 +1,7 @@
 using CSV, DataFrames
 using MomentOpt, MosekTools
 using DynamicPolynomials
+using LinearAlgebra
 using PGFPlots
 
 D = CSV.read("vehicle_tracks_000.csv", DataFrame) |>
@@ -10,5 +11,25 @@ D = CSV.read("vehicle_tracks_000.csv", DataFrame) |>
     (d -> filter(e -> 0 <= e["x"] <= 1, d)) |>
     (d -> filter(e -> 0 <= e["y"] <= 1, d))
 
-p = Plots.Scatter(D[:,["x","y"]])
-save("merge.pdf",p)
+@polyvar x[1:4]
+d = 3
+ϕ = monomials(x[1:2],0:2d)
+ρ0 = DiracMeasure(x,[0.5,0.0,0.0,0.0])
+ρT = DiracMeasure(x,[0.0,0.6,0.0,0.0])
+M = sum(DiracMeasure(x,collect(s)) for s in eachrow(D)) * (1/size(D,1))
+Λ = let v = monomials(x,0:d)
+    Σ = integrate.(v*v',M)
+    v'*inv(Σ+1e-4I)*v
+end
+
+m = GMPModel(Mosek.Optimizer)
+@variable m ρ Meas(x,support=@set(x'x<=10))
+@objective m Min Mom(Λ,ρ)
+@constraint m Mom.(differentiate(ϕ,x[1:2])*x[3:4],ρ) .== integrate.(ϕ,ρT) - integrate.(ϕ,ρ0)
+optimize!(m)
+
+q = let v = monomials(x[1:2],0:d)
+    Σ = integrate.(v*v',ρ)
+    v'*inv(Σ+1e-4I)*v
+end
+save("merge.pdf", Plots.Image((x...)->1/q(x),(0,1),(0,1)))
