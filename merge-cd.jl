9336a88e1f25461a186aadc0cc7047315d8b3c23
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2025-07-03 17:28:06 +0200

    cd kernel

--- merge-cd.jl
+++ merge-cd.jl
@@ -1,4 +1,5 @@
 using CSV, DataFrames
+using MultivariateStats
 using MomentOpt, MosekTools
 using DynamicPolynomials
 using LinearAlgebra
@@ -12,25 +13,32 @@ D = CSV.read("vehicle_tracks_000.csv", DataFrame) |>
     (d -> filter(e -> 0 <= e["y"] <= 1, d))
 
 @polyvar x[1:4]
-d = 4
-ϕ = monomials(x[1:2],0:2d)
 ρ0 = DiracMeasure(x,[0.5,0.0,0.0,0.0])
 ρT = DiracMeasure(x,[0.0,0.6,0.0,0.0])
 M = sum(DiracMeasure(x,collect(s)) for s in eachrow(D)) * (1/size(D,1))
-Λ = let v = monomials(x,0:d)
-    Σ = integrate.(v*v',M)
-    v'*inv(Σ+1e-4I)*v
-end
 
-Σ = integrate.(ϕ*ϕ',M)
-F = svd(Σ)
-N = 5
+d = 3
+ϕ = monomials(x[1:2],0:2d)
+K = let Σ = Symmetric(integrate.(ϕ*ϕ',M))
+    iΣ = inv(Σ+1e-4I)
+    cholesky(iΣ).U
+end
+Φ(s) = let v = [e(s) for e in ϕ]
+    K*v
+end
+Q(x,y) = Φ(x)'*Φ(y)
+Λ = ϕ'*K'*K*ϕ
+
+F = fit(KernelPCA, Float64.(stack(eachrow(D[1:200,1:2]))), kernel=Q)
+DΦ = stack(Φ.(collect.(eachrow(D[1:200,1:2]))))
+U = DΦ*projection(F)
+UC = nullspace(U')
 
 m = GMPModel(Mosek.Optimizer)
 @variable m ρ Meas(x,support=@set(x'x<=10))
 @objective m Min Mom(Λ,ρ)
-@constraint m F.U[:,1:N]'*Mom.(differentiate(ϕ,x[1:2])*x[3:4],ρ) .== F.U[:,1:N]'*(integrate.(ϕ,ρT) - integrate.(ϕ,ρ0))
-@constraint m F.U[:,N+1:end]'*Mom.(ϕ,ρ) .== 0
+@constraint m U' *Mom.(differentiate(ϕ,x[1:2])*x[3:4],ρ) .== U'*(integrate.(ϕ,ρT) - integrate.(ϕ,ρ0))
+@constraint m UC'*Mom.(ϕ,ρ) .== 0
 optimize!(m)
 
 q = let v = monomials(x[1:2],0:d)
