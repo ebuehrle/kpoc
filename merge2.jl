f9418bf70becb0c5be78114e1787b87e6c00f99b
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2025-07-04 08:53:53 +0200

    pair

---
+++ merge2.jl
@@ -0,0 +1,46 @@
+using CSV, DataFrames
+using MomentOpt, MosekTools
+using DynamicPolynomials
+using LinearAlgebra
+using PGFPlots
+
+D = CSV.read("vehicle_tracks_000.csv", DataFrame) |>
+    (d -> d[:,["x","y","vx","vy"]]) |>
+    (d -> d .- [1000 1000 0 0]) |>
+    (d -> d ./ 20) |>
+    (d -> filter(e -> 0 <= e["x"] <= 1, d)) |>
+    (d -> filter(e -> 0 <= e["y"] <= 1, d))
+
+@polyvar x[1:4]
+d = 3
+ϕ = monomials(x[1:2],0:2d)
+ρ0 = DiracMeasure(x,[1.0,0.4,0.0,0.0])
+ρT = DiracMeasure(x,[0.2,0.6,0.0,0.0])
+M = sum(DiracMeasure(x,collect(s)) for s in eachrow(D)) * (1/size(D,1))
+Λ = let v = monomials(x,0:d)
+    Σ = integrate.(v*v',M)
+    v'*inv(Σ+1e-4I)*v
+end
+
+Σ = integrate.(ϕ*ϕ',M)
+F = svd(Σ)
+N = 25
+
+m = GMPModel(Mosek.Optimizer)
+@variable m ρ Meas(x,support=@set(x'x<=10))
+@objective m Min Mom(Λ,ρ)
+@constraint m F.U[:,1:N]'*Mom.(differentiate(ϕ,x[1:2])*x[3:4],ρ) .== F.U[:,1:N]'*(integrate.(ϕ,ρT) - integrate.(ϕ,ρ0))
+@constraint m F.U[:,N+1:end]'*Mom.(ϕ,ρ) .== 0
+optimize!(m)
+
+q = let v = monomials(x[1:2],0:d)
+    Σ = integrate.(v*v',ρ)
+    v'*inv(Σ+1e-4I)*v
+end
+save("merge2.pdf", Axis([
+    Plots.Image((x...)->1/q(x),(0,1),(0,1)),
+    Plots.Quiver(
+        D[1:10:end,"x"],D[1:10:end,"y"],
+        D[1:10:end,"vx"]/10,D[1:10:end,"vy"]/10,
+        style="-stealth,blue,no markers"),
+],xmin=0,xmax=1,ymin=0,ymax=1))
